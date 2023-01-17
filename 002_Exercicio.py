3778565ebbcbccf0583d1973fe691ae79759caf0
Leonardo Ferreira <leonardo.ferreira01@fatec.sp.gov.br>
2023-01-17 19:35:33 -0300

    Add files via upload

---
+++ 002_Exercicio.py
@@ -0,0 +1,9 @@
+##############################################################################
+# Autor: Leonardo Ferreira
+# Aumento de salário
+##############################################################################
+
+salario = float(input("Digite o salario: "))
+aumento = float(input("Qual o aumento desejado: "))
+
+print("O aumento foi de: ", salario + (salario*aumento)/100)
